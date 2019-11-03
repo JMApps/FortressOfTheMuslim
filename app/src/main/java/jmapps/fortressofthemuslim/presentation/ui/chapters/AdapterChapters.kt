60d5ac1f1c0cad1c42ba7f05cc17d839784e2876
dev_muslim <jmappsmuslim@gmail.com>
2019-11-03 17:19:00 +0300

    Added filterable with chapters

--- app/src/main/java/jmapps/fortressofthemuslim/presentation/ui/chapters/AdapterChapters.kt
+++ app/src/main/java/jmapps/fortressofthemuslim/presentation/ui/chapters/AdapterChapters.kt
@@ -1,17 +1,26 @@
 package jmapps.fortressofthemuslim.presentation.ui.chapters
 
+import android.annotation.SuppressLint
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import jmapps.fortressofthemuslim.R
 
-class AdapterChapters(private val chapterList: MutableList<ModelChapters>):
-    RecyclerView.Adapter<ViewHolderChapters>() {
+class AdapterChapters(private var chapterList: MutableList<ModelChapters>) :
+    RecyclerView.Adapter<ViewHolderChapters>(), Filterable {
+
+    private var mainChapterList: MutableList<ModelChapters>? = null
+
+    init {
+        mainChapterList = chapterList
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderChapters {
         return ViewHolderChapters(LayoutInflater.from(parent.context).inflate(
-            R.layout.item_chapter, parent, false))
+                R.layout.item_chapter, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -24,5 +33,34 @@ class AdapterChapters(private val chapterList: MutableList<ModelChapters>):
 
         holder.tvChapterNumber.text = chapterId.toString()
         holder.tvChapterTitle.text = Html.fromHtml(strChapterTitle)
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            @SuppressLint("DefaultLocale")
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charString = charSequence.toString()
+                chapterList = if (charString.isEmpty()) {
+                    mainChapterList as MutableList<ModelChapters>
+                } else {
+                    val filteredList = ArrayList<ModelChapters>()
+                    for (row in mainChapterList!!) {
+                        if (row.strChapterTitle?.toLowerCase()!!.contains(charString.toLowerCase()) ||
+                            row.chapterId.toString().contains(charSequence)) {
+                            filteredList.add(row)
+                        }
+                    }
+                    filteredList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = chapterList
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                chapterList = filterResults.values as ArrayList<ModelChapters>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
--- app/src/main/java/jmapps/fortressofthemuslim/presentation/ui/chapters/FragmentChapters.kt
+++ app/src/main/java/jmapps/fortressofthemuslim/presentation/ui/chapters/FragmentChapters.kt
@@ -1,10 +1,13 @@
 package jmapps.fortressofthemuslim.presentation.ui.chapters
 
+import android.app.SearchManager
+import android.content.Context.SEARCH_SERVICE
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.TextUtils
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import jmapps.fortressofthemuslim.R
@@ -12,7 +15,7 @@ import jmapps.fortressofthemuslim.data.database.DatabaseLists
 import jmapps.fortressofthemuslim.data.database.DatabaseOpenHelper
 import kotlinx.android.synthetic.main.fragment_chapters.view.*
 
-class FragmentChapters : Fragment() {
+class FragmentChapters : Fragment(), SearchView.OnQueryTextListener {
 
     private lateinit var rootChapters: View
 
@@ -20,9 +23,13 @@ class FragmentChapters : Fragment() {
     private lateinit var chapterList: MutableList<ModelChapters>
     private lateinit var adapterChapters: AdapterChapters
 
+    private var searchView: SearchView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         rootChapters = inflater.inflate(R.layout.fragment_chapters, container, false)
+
+        setHasOptionsMenu(true)
 
         database = DatabaseOpenHelper(context).readableDatabase
         chapterList = DatabaseLists(context).getChapterList
@@ -34,5 +41,28 @@ class FragmentChapters : Fragment() {
         rootChapters.rvMainChapters.adapter = adapterChapters
 
         return rootChapters
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_chapters, menu)
+        val searchManager = context?.getSystemService(SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.action_search_chapter).actionView as SearchView?
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+        searchView?.maxWidth = Integer.MAX_VALUE
+        searchView?.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        if (TextUtils.isEmpty(newText)) {
+            adapterChapters.filter.filter("")
+        } else {
+            adapterChapters.filter.filter(newText)
+        }
+        return true
     }
 }
